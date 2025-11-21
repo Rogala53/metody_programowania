9720b5299e2d642e9fbf6e17a608a3bf64d6d963
Łukasz Rogala <lukrog4@gmail.com>
2025-11-21 10:59:59 +0100

    NotFoundError added

--- src/classes/services/FlightService.ts
+++ src/classes/services/FlightService.ts
@@ -1,39 +1,50 @@
-import {Flight} from "../Flight.ts";
+import type {IFlight} from "../../interfaces/IFlight.ts";
 import type {FlightSearchCriteria} from "../../types/FlightSearchCriteria.ts";
 import type {IFlightDbService} from "../../interfaces/IFlightDbService.ts";
-import type {IFlightService} from "../../interfaces/IlightService.ts";
+import type {IFlightService} from "../../interfaces/IFlightService.ts";
 
 export class FlightService implements IFlightService {
     private db: IFlightDbService;
     constructor(db: IFlightDbService) {
         this.db = db;
     }
-    async findFlights(criteria: FlightSearchCriteria): Promise<Flight[]> {
+    async findFlights(criteria: FlightSearchCriteria): Promise<IFlight[]> {
         if(criteria.passengerCount <= 0) {
             throw new Error("Liczba pasażerów musi być dodatnia");
         }
         return this.db.findFlights(criteria);
 
     }
-    async getFlightDetails(flightId: number): Promise<Flight | null> {
-        return this.db.findFlightById(flightId);
+    async getFlightDetails(flightId: number): Promise<IFlight> {
+        const flight: IFlight = this.db.findFlightById(flightId);
+        if(!flight) {
+            throw new NotFoundError("Lot o takim id nie istnieje");
+        }
+        return flight;
     }
     async updateAvailableSeats(flightId: number, seats: number): Promise <boolean> {
-        const flight = await this.db.findFlightById(flightId);
-        if(!flight) {
-            throw new Error("Lot nie istnieje");
-        }
-        let newSeats: number;
-        if(seats > 0) {
-            newSeats = flight.availableSeats - seats;
-            if(newSeats < 0) {
-                throw new Error("Brak wystarczającej liczby miejsc");
+        let newSeats: number = 0;
+        try {
+            const flight = await this.db.findFlightById(flightId);
+            if(!flight) {
+                throw new Error("Lot nie istnieje");
             }
-        }
-        else {
-            newSeats = flight.availableSeats - seats;
+            if(seats > 0) {
+                newSeats = flight.availableSeats - seats;
+                if(newSeats < 0) {
+                    throw new Error("Brak wystarczającej liczby miejsc");
+                }
+            }
+            else {
+                newSeats = flight.availableSeats - seats;
+            }
+
+        } 
+        catch(error) {
+
         }
         return this.db.updateFlightSeats(flightId, newSeats);
+        
     }
 
 
